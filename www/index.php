60c6ed135da3cc5d338fbc50a19afc79117f1cd5
iBaiYang <zhuyingu@126.com>
2017-07-17 17:31:26 +0800

    自动生成Admin模块并绑定到当前入口文

--- www/index.php
+++ www/index.php
@@ -17,6 +17,11 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG', true);
 
+// 自动生成Admin模块并绑定到当前入口文件
+//define('BIND_MODULE', 'Admin');
+//define('BUILD_CONTROLLER_LIST', 'Index, Common');
+//define('BUILD_MODEL_LIST', 'Admin');
+
 // 定义应用目录
 define('APP_PATH', '../Application/');
 
